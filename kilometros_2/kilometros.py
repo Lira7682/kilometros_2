3495a310c81974255704433b7f130f9723144185
Lira7682 <a.23308051281191@cbtis128.edu.mx>
2024-09-23 20:42:09 -0600

    Add files via upload

---
+++ kilometros_2/kilometros.py
@@ -0,0 +1,11 @@
+print (" ") #Espacio a agregar
+print ("Lira Hernandez Dayana Yamileth: programa que muestra consumo de combustible")
+print (" ")
+
+a = int(input("Ingresa la cantidad de kilometros que recorrio la motocicleta"))  #Instruccion para ingresar los kilometros
+b = int(input("Ingresa la cantidad de litros que consumio"))  #Instruccion para ingresar litros
+
+division = b/a   #Operacion a realizar
+print ("El resultado de la division es:", division)  #Resultado de la operacion
+
+print (" ")  #Espacio a agregar
